b221d5af11b8dc5350ad1df3a6dcee66fec3f451
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-05-01 19:41:44 +0300

    Initial commit

---
+++ src/main/java/com/example/hw_2_11_string/Hw211StringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_11_string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211StringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw211StringApplication.class, args);
+	}
+
+}
